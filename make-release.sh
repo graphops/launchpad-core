2fa38e2f6991a0da86bc5b0f7842f808c8754db4
Chris Wessels <undefined.za@gmail.com>
2022-08-30 14:58:14 +0100

    feat: clean up for incremental release

---
+++ make-release.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -ex
+
+TAG="$1"; shift
+
+if [ -z "$TAG" ]; then
+    echo "Missing tag arg"
+fi
+
+git checkout feature/v1
+git merge main
+if [ -n "$TAG" ]; then
+    git tag $TAG
+    git push --tags
+fi
+git push feature/v1
