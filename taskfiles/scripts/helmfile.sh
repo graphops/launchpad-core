4e6aef4d52ef81ff90a14f486811dd5aba6c467a
Chris Wessels <undefined.za@gmail.com>
2022-09-10 21:25:52 -0400

    fix(helmfiles): disable validation on install for kube-prometheus-stack

--- taskfiles/scripts/helmfile.sh
+++ taskfiles/scripts/helmfile.sh
@@ -9,11 +9,12 @@ set -e
 NS_DIR="$(pwd)/helmfiles/namespace-releases"
 
 usage(){
-    echo "Invalid usage.\n"
+    echo "Invalid usage."
+    echo
     echo "Arguments:"
     echo "<command> The Helmfile command to use. Current value is $([ -z "$COMMAND" ] && echo ❌ Missing || echo ✅ $COMMAND)."
     echo "<namespace> Must be a file that exists in $NS_DIR. Current value is $([ -z "$NAMESPACE" ] && echo ❌ Missing || echo ✅ $NAMESPACE)."
-    echo "[release-name] Optionally select a single release\n"
+    echo "[release-name] Optionally select a single release"
     exit 1
 }
 
@@ -34,9 +35,12 @@ if [ "$LAUNCHPAD_VERBOSE_LOGS" = "true" ]; then
 fi
 
 if [ "$NAMESPACE" = "sealed-secrets" ]; then
-    gum confirm \
-	--prompt.foreground "#ff0000" \
-	"You are about to modify the $NAMESPACE namespace. Deleting the sealed-secrets controller will render all existing SealedSecrets invalid. Make sure you have backup copies of all your secrets. Are you sure you want to continue?"   
+    TEXT="You are about to modify the $NAMESPACE namespace.
+
+Deleting the sealed-secrets controller will render all existing SealedSecrets invalid.
+
+Make sure you have backup copies of all your secrets. Are you sure you want to continue?"
+    gum confirm	--prompt.foreground "#ff0000" "$TEXT"
 fi
 
 set -x
